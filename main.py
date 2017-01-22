c3cea314226cc511a2fc5f31453492a1a393e051
Tricia Gray <tricia819@gmail.com>
2017-01-21 20:36:49 -0600

    Implemented random fortune and number generators

--- main.py
+++ main.py
@@ -15,10 +15,40 @@
 # limitations under the License.
 #
 import webapp2
+import random
+
+def getRandomFortune():
+
+    fortunes = [
+    "You are a coding diety",
+    "The world needs more people like you.",
+    "Impress yourself.",
+    "You will succeed at anything you do... so long as that thing is related to yarn or soap.",
+    "You will receive a promotion soon."
+    ]
+
+    index = random.randint(0, 4)
+
+
+    return fortunes[index]
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write('Hello world!')
+        header = "<h1>Fortune Cookie</h1>"
+
+        fortune = "<strong>" + getRandomFortune() + "</strong>"
+        fortune_generator = 'Your fortune is: ' + fortune
+        fortune_section = "<p>" + fortune_generator + "</p>"
+
+        lucky_number = "<strong>" + str(random.randint(1, 100)) + "<strong>"
+        number_generator = 'Your lucky number is: ' + lucky_number
+        number_section = "<p>" + number_generator + "</p>"
+
+        new_cookie_button = "<a href='.'><button>Give me another fortune cookie, please!</button></a>"
+
+        content = header + fortune_section + number_section + new_cookie_button
+
+        self.response.write(content)
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
